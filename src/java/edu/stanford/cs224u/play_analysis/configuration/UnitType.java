3b533b21660df460040c1bc63f6a083dccf0b3f7
Vivek Nair <vnair611@stanford.edu>
2013-03-03 17:02:07 -0800

    Added more methods to the Play and Actions interfaces

--- src/java/edu/stanford/cs224u/play_analysis/configuration/UnitType.java
+++
@@ -1,6 +0,0 @@
-import java.io.*;
-import java.util.*;
-
-public enum UnitType {
-	DEFAULT, SCENE, ACT
-}
--- src/quote/Quote.java
+++ src/quote/Quote.java
@@ -2,11 +2,17 @@ package quote;
 
 import java.util.*;
 import character.Character;
+import action.Action;
 
 public class Quote {
 	private static String rawQuote;
 	private static Character speaker;
-	private static ArrayList<Character> mentionedCharacters; 
+	private static ArrayList<Action> containedActions = new ArrayList<Action>();
+	private static ArrayList<Character> mentionedCharacters = new ArrayList<Character>();
+	
+	// Tracks the token location in the quote up to which we
+	// have parsed
+	private static int quoteCursor = 0;
 	
 	private String parsedQuote() {
 		return null;
@@ -20,9 +26,18 @@ public class Quote {
 		return new ArrayList<Character>();
 	}
 	
+	private ArrayList<Action> parsedActions() {
+		return new ArrayList<Action>();
+	}
+	
+	private ArrayList<Action> getActions() {
+		return containedActions;
+	}
+	
 	public Quote(String rawQuote) {
 		this.rawQuote = parsedQuote();
 		this.speaker = parsedSpeaker();
 		this.mentionedCharacters = parsedMentionChars();
+		this.containedActions = parsedActions();
 	}
 }
