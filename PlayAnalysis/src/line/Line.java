287507861d74bae6d20f5b2a0035345c38f17287
Vivek Nair <vnair611@stanford.edu>
2013-03-07 21:27:21 -0800

    Changed the class to an abstract class

--- PlayAnalysis/src/line/Line.java
+++ PlayAnalysis/src/line/Line.java
@@ -1,5 +1,22 @@
 package line;
 
-public class Line {
+import time.TimeUnit; 
+
+public abstract class Line {
 	// Define some shared methods between the Quote and StageDirection classes.
+	private String description = null;
+
+	public String getDescription() {
+		return description; 
+	}
+	
+	// This logic is not specific to either Quote or
+	// StageDirection
+	public TimeUnit returnLocation() {
+		return null; 
+	}
+	
+	public Line(String description) {
+		this.description = description; 
+	}
 }
