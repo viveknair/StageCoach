a34321b97a1da6bbe2838ddf4412f57806d2fb22
Vivek Nair <vnair611@stanford.edu>
2013-03-08 20:34:03 -0800

    Renamed the QuoteExt2 to something more understandable. Also, I modified the play class such that quotes are added to the Character to which it belongs.

---
+++ PlayAnalysis/src/tests/unit/CharacterUnit.java
@@ -0,0 +1,15 @@
+package tests.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import character.Character; 
+
+public class CharacterUnit {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
