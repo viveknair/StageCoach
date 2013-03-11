e0f5df2f69ccbe6920797784573fb6a896f0b354
Vivek Nair <vnair611@stanford.edu>
2013-03-11 02:43:53 -0700

    restructued the visuals folder

--- scripts/convertToJSON.rb
+++ scripts/convertToJSON.rb
@@ -1,27 +1,49 @@
 def convertToJSON
 
   characters = [
-      "NAZAROVNA and EFIMOVNA",
-      "EGOR MERIK",
-      "BORTSOV'S WIFE'S COACHMAN",
-      "FEDYA",
-      "KUSMA",
-      "TIHON EVSTIGNEYEV",
-      "SAVVA",
-      "MARIA EGOROVNA"
+    "Ghost of Hamlet Father",
+    "Marcellus",
+    "Osric",
+    "Ophelia",
+    "Guildenstern",
+    "I Player",
+    "Lords",
+    "Claudius",
+    "Horatio",
+    "Polonius",
+    "1 Clown",
+    "Messengers",
+    "Rosencrantz",
+    "Laertes",
+    "Gertrude",
+    "Sailors",
+    "Marcellus",
+    "Bernardo",
+    "Fortinbras",
+    "Laertes",
+    "Francisco",
+    "A Gentleman",
+    "Voltimand",
+    "Reynaldo",
+    "A Captain.",
+    "2 Clown"
   ]
   
   indexHash = Hash[characters.map.with_index.to_a]
 
   puts indexHash.to_s
   str = ""
-  lines = IO.readlines("adjacency_output_chekhov") 
+  lines = IO.readlines(ARGV[0]) 
   parsedJSON = ""
   currentSource = -1 
   lines.each do |line|
      tokens = line.split(",")
      puts tokens.size
      if tokens.size == 1
+        puts "Switching sources to #{tokens[0].strip}"
+        if not indexHash[tokens[0].strip]
+          puts "This one doesn't exist"
+        end
        currentSource = indexHash[tokens[0].strip]
      else 
        str = "{source:#{currentSource.to_s}, target:#{indexHash[tokens[0].strip].to_s}, value:#{tokens[1].strip}},\n"
