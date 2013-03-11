017c976429faf456b63ba786988a2572e37f9421
Vivek Nair <vnair611@stanford.edu>
2013-03-10 19:20:43 -0700

    Added in a quick note about the dangers of exposing the GlobalLineNode to the client.

--- PlayAnalysis/src/examples/AdjacencyApproach.java
+++ PlayAnalysis/src/examples/AdjacencyApproach.java
@@ -21,6 +21,53 @@ public class AdjacencyApproach {
 	static HashMap<String, Character> characters = new HashMap<String, Character>();
 	static HashMap <Character, HashMap<Character, Integer>> edgeWeights = new HashMap <Character, HashMap<Character, Integer>>();
 
+	private static void previousQuoteAnalysis(Character currentCharacter, GlobalLineNode prevNodeQuote, Quote previousQuote, int totalTokenCount) {
+		if (prevNodeQuote != null) { 
+			Line previousLine = prevNodeQuote.getLine();
+			if (previousLine.getType() == LineType.QUOTE) {
+				previousQuote = (Quote) previousLine; 
+			}
+			if (previousQuote != null) {
+				// System.out.println("Previous node is not null");
+				System.out.println("The total token count is " + totalTokenCount);
+				HashMap<Character, Integer> characterMap = edgeWeights.get(currentCharacter);
+				if (characterMap == null) {
+					characterMap = new HashMap<Character, Integer>();
+				}
+				Integer characterCount = characterMap.get(previousQuote.getCharacter());
+				if (characterCount != null) {
+					characterMap.put(previousQuote.getCharacter(), characterCount.intValue() + totalTokenCount);
+				} else {
+					characterMap.put(previousQuote.getCharacter(), totalTokenCount);
+				}
+				edgeWeights.put(currentCharacter, characterMap);
+			}
+		}
+	}
+	
+	private static void nextQuoteAnalysis(Character currentCharacter, GlobalLineNode nextNodeQuote, Quote nextQuote, Quote previousQuote, int totalTokenCount) {
+		if (nextNodeQuote != null) {
+			Line nextLine = nextNodeQuote.getLine();
+			if (nextLine.getType() == LineType.QUOTE) {
+				nextQuote = (Quote) nextLine; 
+			}
+			if ((nextQuote != null && previousQuote != null && nextQuote.getCharacter() != previousQuote.getCharacter()) 
+				 || (nextQuote != null)) {
+				HashMap<Character, Integer> characterMap = edgeWeights.get(currentCharacter);
+				if (characterMap == null) {
+					characterMap = new HashMap<Character, Integer>();
+				}
+				Integer characterCount = characterMap.get(nextQuote.getCharacter());
+				if (characterCount != null) {
+					characterMap.put(nextQuote.getCharacter(), characterCount.intValue() + totalTokenCount);
+				} else {
+					characterMap.put(nextQuote.getCharacter(), totalTokenCount);
+				}
+				edgeWeights.put(currentCharacter, characterMap);
+			}
+		}
+	}
+	
 	private static void constructAdjacency() {
 		characters = currentPlay.returnCharacters(null, null);
 		for (String charName : characters.keySet()) {
@@ -28,57 +75,18 @@ public class AdjacencyApproach {
 			ArrayList<Quote> quotes = currentCharacter.getQuotes();
 			for (Quote quote : quotes) {
 				int totalTokenCount = quote.getTotalTokens();
-				System.out.println("Character " + charName + " has " + quotes.size() + " quotes.");
+				
+				// The fact that the GlobalLineNode is actually exposed to the client
+				// is troubling. We have to refactor this design substantially.
 				GlobalLineNode nodeQuote = quote.getGllNode();
 				GlobalLineNode prevNodeQuote = nodeQuote.getPreviousNode(LineNodeHeader.DEFAULT);
 				GlobalLineNode nextNodeQuote = nodeQuote.getNextNode(LineNodeHeader.DEFAULT);
-				// System.out.println("prevNodeQuote is " + prevNodeQuote + " and nextNodeQuote is " + nextNodeQuote);
 				
 				Quote previousQuote = null;
 				Quote nextQuote = null;
 				
-				if (prevNodeQuote != null) { 
-					Line previousLine = prevNodeQuote.getLine();
-					if (previousLine.getType() == LineType.QUOTE) {
-						previousQuote = (Quote) previousLine; 
-					}
-					if (previousQuote != null) {
-						// System.out.println("Previous node is not null");
-						System.out.println("The total token count is " + totalTokenCount);
-						HashMap<Character, Integer> characterMap = edgeWeights.get(currentCharacter);
-						if (characterMap == null) {
-							characterMap = new HashMap<Character, Integer>();
-						}
-						Integer characterCount = characterMap.get(previousQuote.getCharacter());
-						if (characterCount != null) {
-							characterMap.put(previousQuote.getCharacter(), characterCount.intValue() + totalTokenCount);
-						} else {
-							characterMap.put(previousQuote.getCharacter(), totalTokenCount);
-						}
-						edgeWeights.put(currentCharacter, characterMap);
-					}
-				}
-				
-				if (nextNodeQuote != null) {
-					Line nextLine = nextNodeQuote.getLine();
-					if (nextLine.getType() == LineType.QUOTE) {
-						nextQuote = (Quote) nextLine; 
-					}
-					if ((nextQuote != null && previousQuote != null && nextQuote.getCharacter() != previousQuote.getCharacter()) 
-						 || (nextQuote != null)) {
-						HashMap<Character, Integer> characterMap = edgeWeights.get(currentCharacter);
-						if (characterMap == null) {
-							characterMap = new HashMap<Character, Integer>();
-						}
-						Integer characterCount = characterMap.get(nextQuote.getCharacter());
-						if (characterCount != null) {
-							characterMap.put(nextQuote.getCharacter(), characterCount.intValue() + totalTokenCount);
-						} else {
-							characterMap.put(nextQuote.getCharacter(), totalTokenCount);
-						}
-						edgeWeights.put(currentCharacter, characterMap);
-					}
-				}
+				previousQuoteAnalysis(currentCharacter, prevNodeQuote, previousQuote, totalTokenCount);
+				nextQuoteAnalysis(currentCharacter, nextNodeQuote, nextQuote, previousQuote, totalTokenCount);
 			}
 		}
 	}
